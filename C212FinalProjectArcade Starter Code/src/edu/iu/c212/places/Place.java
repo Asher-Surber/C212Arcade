ac41f61b1f936a8193e877c61b155f28ce10c340
Asher Surber <risurb@iu.edu>
2021-12-06 14:42:10 -0500

    Place and Lobby completed, Store initialized

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/Place.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/Place.java
@@ -1,6 +1,19 @@
 package edu.iu.c212.places;
 
+import edu.iu.c212.Arcade;
+import edu.iu.c212.models.User;
+
 public abstract class Place {
 
+    String placeName;
+    Arcade arcade;
+    double entryFee;
+    boolean isGame;
 
+    abstract void onEnter(User user);
+
+    @Override
+    public String toString(){
+        return placeName + "- $" + entryFee + ", Game? " + isGame;
+    }
 }
---
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/places/Store.java
@@ -0,0 +1,10 @@
+package edu.iu.c212.places;
+
+import edu.iu.c212.models.User;
+
+public class Store extends Place{
+    @Override
+    void onEnter(User user) {
+
+    }
+}
