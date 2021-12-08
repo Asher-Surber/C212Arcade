af976dc8219b2bf67e19da11ced10ac537435a7d
Asher Surber <risurb@iu.edu>
2021-12-08 13:54:40 -0500

    getUserData implemented

--- C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/FileUtils.java
+++ C212FinalProjectArcade Starter Code/src/edu/iu/c212/utils/FileUtils.java
@@ -1,13 +1,14 @@
 package edu.iu.c212.utils;
 
+import edu.iu.c212.models.Item;
 import edu.iu.c212.models.User;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class FileUtils {        //TODO
+public class FileUtils {
     private static File file = new File("src/edu.iu.c212.utils.userData.txt");
 
     // line format:
@@ -21,14 +22,40 @@ public class FileUtils {        //TODO
      */
     public static void writeUserDataToFile(List<User> users) throws IOException {
         FileWriter w = new FileWriter(file);
-        for (int i = 0; i < users.size(); i++){
-            
+        for (User user : users) {
+            w.write(user.toString() + "\n");
         }
+        w.close();
     }
 
     /**
      * Read user data from the file you provided above. Return a list of Users
      */
     public static List<User> getUserDataFromFile() throws IOException {
+        FileReader fr = new FileReader(file);
+        LineNumberReader lr = new LineNumberReader(fr);
+        String l;
+        ArrayList<User> userList = new ArrayList<>();
+        ArrayList<Item> itemList = new ArrayList<>();
+        Collections.addAll(itemList, Item.values());
+        do{
+            l = lr.readLine();
+            String[] splitStr = l.split("\\|");
+            ArrayList<Item> inventory = new ArrayList<>();
+            String[] itemArr = splitStr[2].split(",");
+            for (int i = 0; i < 3; i++){
+                for(Item j : itemList){
+                    if(itemArr[i].equals(j.name())){
+                        inventory.add(j);
+                        break;
+                    }
+                }
+            }
+            User newUser = new User(splitStr[0], Double.parseDouble(splitStr[1]), inventory);
+            userList.add(newUser);
+        }
+        while (!l.equals(""));
+
+        return userList;
     }
 }
