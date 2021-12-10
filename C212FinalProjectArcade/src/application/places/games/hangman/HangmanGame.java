638fc319651a275a2316f17b5d184a376b96a3cb
Asher Surber <risurb@iu.edu>
2021-12-10 01:16:36 -0500

    progress on hangman

--- C212FinalProjectArcade/src/application/places/games/hangman/HangmanGame.java
+++ C212FinalProjectArcade/src/application/places/games/hangman/HangmanGame.java
@@ -2,9 +2,13 @@ package application.places.games.hangman;
 
 import application.models.User;
 import application.places.games.Game;
+import application.utils.ConsoleUtils;
+import application.utils.http.HttpUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class HangmanGame extends Game implements IHangmanGame {
 
@@ -39,6 +43,27 @@ public class HangmanGame extends Game implements IHangmanGame {
 
     @Override
     protected void onEnter(User user) {
+        System.out.print("Welcome to Hangman! You will be shown a blurred-out word, and you have 6 tries to guess\neither a letter or the word.\n");
+        System.out.println("If you guess the word, you'll win $15.00! Good luck!");
 
+        try{
+            List<Character> lex = getValidLexicon();
+            String word = HttpUtils.getRandomHangmanWord();
+            List<Character> guesses = new ArrayList<>();
+            getBlurredWord(guesses, word);
+            String guess;
+            do{
+                guess = ConsoleUtils.readLineFromConsole();
+                if(guess.length() == 1){
+                    if(word.contains(guess)){
+                        char lGuess = guess.charAt(0);
+                    }
+                }
+            }
+            while(guesses.size() < 6 && !guess.equalsIgnoreCase(word));
+        }
+        catch(IOException e){
+            System.out.println("Problem getting word");
+        }
     }
 }
