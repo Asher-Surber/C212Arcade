5199ce16c9cdf201d21ec98dc2a9596a33fa215e
Asher Surber <risurb@iu.edu>
2021-12-10 13:01:55 -0500

    blackjack player implemented, not yet tested

--- C212FinalProjectArcade/src/application/places/games/blackjack/BlackjackParticipant.java
+++ C212FinalProjectArcade/src/application/places/games/blackjack/BlackjackParticipant.java
@@ -1,14 +1,35 @@
 package application.places.games.blackjack;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public abstract class BlackjackParticipant {
 
-    protected static ArrayList<String> cards;
+    public static Random rand = new Random();
+
+    protected static ArrayList<String> cards = new ArrayList<>(List.of("A", "A", "A", "A", "2", "2", "2", "2", "3", "3",
+            "3", "3", "4", "4", "4", "4", "5", "5", "5", "5", "6", "6", "6", "6", "7", "7", "7", "7", "8", "8", "8", "8",
+            "9", "9", "9", "9", "10", "10", "10", "10", "K", "K", "K", "K", "Q", "Q", "Q", "Q", "J", "J", "J", "J"));
     protected int[] handTotals;
 
     public void hit() {
+        String newCard = cards.get(rand.nextInt(cards.size()));
+        switch(newCard) {
+            case "A":
+                handTotals[0] += 1;
+                handTotals[1] += 11; break;
 
+            case "K":
+            case "Q":
+            case "J":
+                handTotals[0] += 10;
+                handTotals[1] += 10; break;
+            default:
+                handTotals[0] += Integer.parseInt(newCard);
+                handTotals[1] += Integer.parseInt(newCard);
+        }
+        cards.remove(newCard);
     }
 
     public abstract int getBestTotal();
--- C212FinalProjectArcade/src/application/places/games/blackjack/BlackjackPlayer.java
+++ C212FinalProjectArcade/src/application/places/games/blackjack/BlackjackPlayer.java
@@ -1,11 +1,42 @@
 package application.places.games.blackjack;
 
+import java.util.ArrayList;
+
 public class BlackjackPlayer extends BlackjackParticipant {
 
+    public BlackjackPlayer(){
+        ArrayList<String> deck = cards;
+        hit();
+        hit();
+    }
 
+    public String getCurrentTotalsString(){
+        if(handTotals[0] == handTotals[1]){
+            return "Total: " + handTotals[0];
+        }
+        else if(handTotals[0] <= 21 && handTotals[1] <= 21){
+            return "Totals: " + handTotals[0] + "|" + handTotals[1];
+        }
+        return "Something went wrong with BP.getcurrenttotals";
+    }
 
     @Override
     public int getBestTotal() {
-        return 0;
+        int diff1 = 21 - handTotals[0];
+        int diff2 = 21 - handTotals[1];
+        if(diff1 >= 0 && diff1 < diff2){
+            return handTotals[0];
+        }
+        else if(diff2 >= 0 && diff2 < diff1){
+            return handTotals[1];
+        }
+        else{
+            if(diff1 >= diff2){
+                return diff1;
+            }
+            else{
+                return diff2;
+            }
+        }
     }
 }
